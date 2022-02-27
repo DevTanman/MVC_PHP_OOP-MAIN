d4c6ec27fc824cc53d9d9c986eb8ad13e66593a1
DevTanman <91167443+DevTanman@users.noreply.github.com>
2022-02-27 08:50:37 -0500

    Update Database.php

--- config/Database.php
+++ config/Database.php
@@ -3,14 +3,14 @@
         private $host = 'nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         private $db_name = 'cpb23pb80w70wkw8';
         private $username = 'optgyp1iqzc076og';
-        private $password = getenv('MY_PASS');
+        //private $password = getenv('MY_PASS');
         private $conn;
 
         public function connect() {
             $this->conn = null;
 
             try {
-                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password); 
+                $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, getenv('MY_PASS')); 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 $error_message = 'Database Error: ';
